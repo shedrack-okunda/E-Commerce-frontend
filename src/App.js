cde25a64115fde4c4dbdd20245380aae0c9cd4d0
Shedrack Okunda <okundashedrack@gmail.com>
2024-11-11 18:38:26 +0300

    Initiated the project and installed all the dependencies

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
