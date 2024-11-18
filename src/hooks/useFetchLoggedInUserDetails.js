c9d351b8dfdb6bdb4ec81672f047fd05489fba53
Shedrack Okunda <okundashedrack@gmail.com>
2024-11-18 12:34:27 +0300

    added a function that fetches user information when a user logged in

--- src/hooks/useFetchLoggedInUserDetails.js
+++ src/hooks/useFetchLoggedInUserDetails.js
@@ -2,6 +2,11 @@ import { useEffect } from "react";
 import { selectLoggedInUser } from "../features/auth/AuthSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAddressByUserIdAsync } from "../features/address/AddressSlice";
+import { fetchWishlistByUserIdAsync } from "../features/wishlist/WishlistSlice";
+import { fetchCartByUserIdAsync } from "../features/cart/CartSlice";
+import { fetchAllCategoriesAsync } from "../features/categories/CategoriesSlice";
+import { fetchAllBrandsAsync } from "../features/brands/BrandSlice";
+import { fetchLoggedInUserByIdAsync } from "../features/user/UserSlice";
 
 export const useFetchLoggedInUserDetails = (deps) => {
   const loggedInUser = useSelector(selectLoggedInUser);
@@ -9,11 +14,16 @@ export const useFetchLoggedInUserDetails = (deps) => {
 
   useEffect(() => {
     if (deps && loggedInUser?.isVerified) {
-      dispatch();
+      dispatch(fetchLoggedInUserByIdAsync(loggedInUser?._id));
+      dispatch(fetchAllBrandsAsync());
+      dispatch(fetchAllCategoriesAsync());
 
       if (!loggedInUser.isAdmin) {
+        dispatch(fetchCartByUserIdAsync(loggedInUser?._id));
         dispatch(fetchAddressByUserIdAsync(loggedInUser?._id));
+        dispatch(fetchWishlistByUserIdAsync(loggedInUser?._id));
       }
     }
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deps]);
 };
